eb1bd8a88c911ec55ddf3db5bb3b7edf86d5c100
Caroline <carolinebaptista41@gmail.com>
2018-04-02 21:10:54 -0300

    parte 1- aula 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
-
+import { EnderecoComponent } from './endereco/endereco.component';
+import { EnderecoService } from './services/endereco.service';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnderecoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [EnderecoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
